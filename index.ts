c9e869721522d708d7cc775749c7fbeb7c414ba0
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-07-17 22:27:30 +1200

    Added a dynamic route test

--- index.ts
+++ index.ts
@@ -10,6 +10,11 @@ app.get('/', (req: Request, res: Response): void => {
   res.json('test').status(200)
 })
 
+app.get('/:id', (req: Req, res: Res): void => {
+  const { id } = req.params
+  res.json(id).status(200)
+})
+
 app.listen(port, (): void => {
   console.log(`app running on port:${port}`)
 })
