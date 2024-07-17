3909bb662f40033d8ca2caa07da38f64f35476e5
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-07-18 02:12:28 +1200

    added an endpoint for interacting with the ai

--- ai/agent.ts
+++ ai/agent.ts
@@ -9,11 +9,12 @@ export const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-async function main(): Promise<void> {
+export async function main(prompt: string): Promise<void> {
   const stream = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
-    messages: [{ role: "user", content: "youre a dungeon master for dnd generate me a short start to a journey including any dice rolls you think are necassary" }],
+    messages: [{ role: "user", content: `!!! IMPORTANT - you dont have to always provide a skill check only do so if the player is attempting to do a challenging task - never ever speak for the player or allow the player to do something that couldnt realisticly be done e.g fly without wings or teleport !!! youre a dungeon master responable for creating a story in the style of dnd. I want you to generate short paragraphs and give the player time to create a response on what to do. when ever you recieve a prompt I want you to analyse whether a skill check should be made and also how difficult it is. -- here is the player details: hp = 10, agility = 10, etc, -- promt(${prompt})` }],
     stream: true,
+    max_tokens: 400,
   });
   for await (const chunk of stream) {
     process.stdout.write(chunk.choices[0]?.delta?.content || "");
@@ -29,4 +30,4 @@ export async function generateStory(): Promise<void> {
   console.log(response);
 }
 
-main()
+main('wake up')
