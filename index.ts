542bc000323974ed17ee8808381202a3f552a5cc
shane-curtis <142857801+ShaneSaw1705@users.noreply.github.com>
2024-07-18 14:52:51 +1200

    fixed ai route

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
 import express, { Request, Response } from "express";
-import testRoute from './routes/route'
-import aiRoute from './routes/ai/aiRouter'
+import testRoute from "./routes/route";
+import aiRoute from "./routes/ai/aiRouter";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use('/test', testRoute)
-app.use('/ai', aiRoute)
+app.use("/test", testRoute);
+app.use("/ai", aiRoute);
 
-app.get('/', (req: Request, res: Response): Response => {
-  return res.json('test').status(200)
-})
+app.get("/", (req: Request, res: Response): Response => {
+  return res.json("test").status(200);
+});
 
-app.get('/:id', (req: Request, res: Response): Response => {
-  const { id } = req.params
-  return res.json(id).status(200)
-})
+app.get("/:id", (req: Request, res: Response): Response => {
+  const { id } = req.params;
+  return res.json(id).status(200);
+});
 
 app.listen(port, (): void => {
-  console.log(`app running on port:${port}`)
-})
+  console.log(`app running on port:${port}`);
+});
